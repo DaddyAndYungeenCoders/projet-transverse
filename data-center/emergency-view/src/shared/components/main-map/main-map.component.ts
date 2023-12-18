27e2627c04cb60f6d64a289386afaa5bac8bee42
sspina69 <sandro.spina.pro@gmail.com>
2023-12-18 17:14:42 +0100

    (CSC-33): Init leaflet

---
+++ data-center/emergency-view/src/shared/components/main-map/main-map.component.ts
@@ -0,0 +1,44 @@
+import {AfterViewInit, Component} from '@angular/core';
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {Coordinates} from "../../types/interfaces/Coordinates";
+import * as L from "leaflet";
+
+@Component({
+  selector: 'app-main-map',
+  standalone: true,
+  imports: [
+    NgClass,
+    NgIf,
+    NgForOf
+  ],
+  templateUrl: './main-map.component.html',
+  styleUrl: './main-map.component.scss'
+})
+export class MainMapComponent implements AfterViewInit{
+  map!: L.Map;
+  private defaultZoomLevel = 20;
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.mountMap(); // Creating the map
+  }
+
+  private mountMap() {
+    const parc: Coordinates = {
+      lat: 45.7790285,
+      lng: 4.8755341
+    };
+
+
+    this.map = L.map('map', {
+      center: [parc.lat, parc.lng],
+      zoom: this.defaultZoomLevel
+    });
+
+    const mainLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: 5,
+      maxZoom: 20
+    }).addTo(this.map);
+  }
+}
--- simulator/simulator-view/src/app/app.component.ts
+++ simulator/simulator-view/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainMapComponent } from "../shared/components/main-map/main-map.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainMapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'simulator-view';
 }
