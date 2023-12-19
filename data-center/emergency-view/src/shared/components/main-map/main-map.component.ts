3a53f71c973018a6f13ec319042b9d125a034159
Ceïf <cecece775@gmail.com>
2023-12-19 08:40:29 +0100

    map layer changed

--- data-center/emergency-view/src/shared/components/main-map/main-map.component.ts
+++ data-center/emergency-view/src/shared/components/main-map/main-map.component.ts
@@ -1,24 +1,20 @@
-import {AfterViewInit, Component} from '@angular/core';
-import {NgClass, NgForOf, NgIf} from "@angular/common";
-import {Coordinates} from "../../types/interfaces/Coordinates";
-import * as L from "leaflet";
+import { AfterViewInit, Component } from '@angular/core';
+import { NgClass, NgForOf, NgIf } from '@angular/common';
+import { Coordinates } from '../../types/interfaces/Coordinates';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-main-map',
   standalone: true,
-  imports: [
-    NgClass,
-    NgIf,
-    NgForOf
-  ],
+  imports: [NgClass, NgIf, NgForOf],
   templateUrl: './main-map.component.html',
-  styleUrl: './main-map.component.scss'
+  styleUrl: './main-map.component.scss',
 })
-export class MainMapComponent implements AfterViewInit{
+export class MainMapComponent implements AfterViewInit {
   map!: L.Map;
   private defaultZoomLevel = 20;
 
-  constructor() {}
+  constructor() { }
 
   ngAfterViewInit() {
     this.mountMap(); // Creating the map
@@ -27,18 +23,20 @@ export class MainMapComponent implements AfterViewInit{
   private mountMap() {
     const parc: Coordinates = {
       lat: 45.7790285,
-      lng: 4.8755341
+      lng: 4.8755341,
     };
-
 
     this.map = L.map('map', {
       center: [parc.lat, parc.lng],
-      zoom: this.defaultZoomLevel
+      zoom: this.defaultZoomLevel,
     });
 
-    const mainLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      minZoom: 5,
-      maxZoom: 20
-    }).addTo(this.map);
+    const mainLayer = L.tileLayer(
+      'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png',
+      {
+        minZoom: 5,
+        maxZoom: 20,
+      }
+    ).addTo(this.map);
   }
 }
