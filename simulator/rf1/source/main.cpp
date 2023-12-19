a6f74eeab4c7dc3f3a258fab306ae171715290d5
Maxime DUBOIS <maxime.dubois.1@tpc16.cpe.lan>
2023-12-19 10:37:24 +0100

    feat: AES OK

--- simulator/rf1/source/main.cpp
+++ simulator/rf1/source/main.cpp
@@ -5,80 +5,85 @@
 #include "aes.h"
 #include "aes.c"
 
-#define MAX_TEXT_LENGTH 20
+#define MAX_TEXT_LENGTH 32
 
 ManagedString CODE = "CDJMS:";
 
 
 MicroBit uBit;
-MicroBitI2C i2c(I2C_SDA0, I2C_SCL0);
-MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_DIGITAL_OUT);
 MicroBitSerial serial(USBTX, USBRX);
+uint8_t key[] = "secret key 123";
+// iv : initialisation vector
+uint8_t iv[16] = {0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff};
 
-
-char encryptedText[MAX_TEXT_LENGTH];
-char decryptedText[MAX_TEXT_LENGTH];
-
-int key = 18;
 
 void encrypt_decrypt(uint8_t message[]) {
     struct AES_ctx ctx;
-    uint8_t key[] = "secret key 123";
-    // iv : initialisation vector
-    uint8_t iv[16] = {0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff};
 
     AES_init_ctx_iv(&ctx, key, iv);
-    AES_CTR_xcrypt_buffer(&ctx, message, strlen((char *) message));
+    AES_CTR_xcrypt_buffer(&ctx, message, strlen((char*)message));
 }
 
-//char* decode_RF(ManagedString s){
-//    return decrypt(const_cast<char*>(s.substring(sizeof(CODE), s.length()).toCharArray()), key);
-//}
+
+
+// Vérification que la trame est bien pour nous
+bool check_cdjms(ManagedString s){
+   return s.substring(0,CODE.length()) == CODE;
+}
 
 void send_RF(ManagedString s){
     ManagedString tosend = CODE + s;
     uBit.radio.datagram.send(tosend);
-    serial.send(" send : " + tosend);
-
+    //serial.send(" send : " + tosend);
 }
 
-//void decrypt_RF(ManagedString s){
-//
-//
-//}
+// Décodage des données reçu
+ManagedString decrypt_RF(ManagedString s){
+    uint8_t message[MAX_TEXT_LENGTH];
+    memcpy(message, (const uint8_t *)s.toCharArray(), MAX_TEXT_LENGTH);
+    encrypt_decrypt(message);
+    return ManagedString((char*)message);
+}
+
+ManagedString decode_RF(ManagedString s){
+   return s.substring(CODE.length(), s.length());
+}
+
+
+//Quand on reçoit des données en RF cryptées => on les decrypte
+void onData(MicroBitEvent) {
+    // PacketBuffer buf(BUF_SIZE);
+    ManagedString buf = uBit.radio.datagram.recv();
+    //serial.send("Received data");
+    //serial.send(buf);
+    if (check_cdjms(buf))
+    {
+        ManagedString test = decode_RF(buf);
+        //serial.send("Decoded data");
+        // serial.send(test);
+        ManagedString decrypted = decrypt_RF(decode_RF(buf));
+        serial.send(decrypted);
+    }
+}
 
 void send_encrypt_RF(ManagedString s){
-    serial.send(" send : " + s);
+    serial.send(" message : " + s);
     uint8_t message[MAX_TEXT_LENGTH];
     memcpy(message, (const uint8_t*)s.toCharArray(), MAX_TEXT_LENGTH);
 
     encrypt_decrypt(message);
-    serial.send(" encrypt : " + ManagedString((char*)message));
     //send_RF(ManagedString((char*)s));
-    send_RF((char*)message);
+    send_RF(ManagedString((char*)message));
 
 }
 
-
-bool check_cdjms(ManagedString s) {
-    return s.substring(0, CODE.length()) == CODE;
-}
-
-//void onData(MicroBitEvent) {
-//    // received data from microbit radio
-//    ManagedString recData = uBit.radio.datagram.recv();
-//    if (check_cdjms(recData)) {
-//        char* decryptedText = decrypt_RF(recData);
-//        serial.send(decryptedText);
-//    }
-//}
 
 int main() {
 
     // Initialize the micro:bit runtime.
     uBit.init();
     uBit.serial.baud(115200);
-    //uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
+    uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
 
     uBit.radio.enable();
     uBit.radio.setGroup(57);
@@ -87,22 +92,18 @@ int main() {
     while (1) {
 
         if (uBit.buttonA.isPressed()) {
-            // for testing purpose
-            //char tosend[128];
-            //snprintf(tosend, sizeof(tosend), "bonjour");
-            //uBit.display.scroll("A");
-
             send_encrypt_RF("Hello World !");
         }
 
         if (uBit.buttonB.isPressed()) {
 
-            send_encrypt_RF("encrypt");
+            send_encrypt_RF("I love IOT");
         }
-        ManagedString toRead = serial.read(sizeof(char[128]), ASYNC);
+        //ManagedString toRead = serial.read(sizeof(char[128]), ASYNC);
 
-        uBit.display.scroll(toRead);
+
 
         uBit.sleep(1000);
     }
+    release_fiber();
 }
