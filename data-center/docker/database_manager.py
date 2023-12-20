e42ffba66c6e0e45cd69bb0689e153a344f39a98
daddyornot <damienmailhebiau@gmail.com>
2023-12-20 14:54:49 +0100

    fix: moved venv

--- data-center/docker/database_manager.py
+++ data-center/docker/database_manager.py
@@ -1,4 +1,6 @@
 import os
+from http import client
+
 from dotenv import load_dotenv
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
@@ -15,6 +17,7 @@ class DatabaseManager:
         self.org = os.getenv("INFLUXDB_V2_ORG")
 
     def insert_data(self, data, tag):
+        self.client.write_points(data)
         p = Point("test").tag(tag["label"], tag["value"]).field(data["field"], data["value"])
         self.write_api.write(bucket=self.bucket, record=p)
 
