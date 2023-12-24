560c2bdc06e25d048d193c3056cc9374b58b27ca
daddyornot <damienmailhebiau@gmail.com>
2023-12-24 16:14:51 +0100

    fix: clean code

--- simulator/relay-mqtt-webserver/app/core/config.py
+++ simulator/relay-mqtt-webserver/app/core/config.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import yaml
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
@@ -6,7 +7,7 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 
-# Load topics yaml file
+# -------- TOPICS CONFIGURATION -------- #
 def load_config(config_path, config_name):
     try:
         with open(config_path, 'r') as file:
@@ -16,19 +17,32 @@ def load_config(config_path, config_name):
         print(f"Error loading YAML: {e}")
 
 
+topics = load_config("app/config/topics.yaml", "topics")
+
+# -------------------------------------- #
+
+
+# -------- SETTINGS CONFIGURATION -------- #
 class Settings(BaseSettings):
     app_name: str = "Relay service MQTT - WebServer API"
     debug: bool = False
 
 
 settings = Settings()
+# ---------------------------------------- #
 
+# -------- LOGGER CONFIGURATION -------- #
 logger = logging.getLogger()
 logging.basicConfig(format='[%(levelname)s] %(asctime)s : %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 logger.setLevel(logging.INFO)
+# -------------------------------------- #
 
-topics = load_config("app/config/topics.yaml", "topics")
 
+# -------- API CONFIGURATION -------- #
+API_URL = os.getenv("API_URL") + os.getenv("API_PORT")
+# ----------------------------------- #
+
+# -------- MQTT CONFIGURATION -------- #
 MQTT_CLIENT_NAME = "relay-mqtt-webserver"
 FIRST_RECONNECT_DELAY = 1
 RECONNECT_RATE = 2
@@ -41,3 +55,4 @@ TOPICS_TO_SUBSCRIBE = [
     (topics.get("simulator.new-sensor-value"), QOS),
     (topics.get("simulator-view.sensor-changed"), QOS)
 ]
+# -------------------------------------- #
