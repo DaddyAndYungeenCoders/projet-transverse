26492629453a2c8a4433e57f75a743d720583c34
daddyornot <damienmailhebiau@gmail.com>
2024-01-11 22:34:14 +0100

    --amend

--- simulator/relay-mqtt-webserver/app/core/config_vars.py
+++ simulator/relay-mqtt-webserver/app/core/config_vars.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # -------- API CONFIGURATION -------- #
 API_URL = os.getenv("API_URL") + ":" + os.getenv("API_PORT") + "/api"
-NEW_AUTO_FIRE = "/fire-event/create"
+NEW_AUTO_FIRE = "/fire-event/create/generator"
 NEW_SENSOR_VALUES = "/sensor/update/%s"
 # ----------------------------------- #
 
